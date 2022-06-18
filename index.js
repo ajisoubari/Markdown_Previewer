70711826e3d36fcbadbb7b457c5cc3ef4ef91ce2
ajisoubari <72975317+ajisoubari@users.noreply.github.com>
2022-06-18 17:17:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ marked.setOptions({
 const renderer = new marked.Renderer();
 
 function App() {
-    const [text, setText] = React.useState("");
+    const [text, setText] = React.useState(qwerty);
     
     return (
         <div className="text-center px-4">
             <h1 className="p-4">My Markdown Previewer</h1>
             <textarea
                 name="text"
-                id="text"
+                id="editor"
                 rows="10"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
@@ -23,6 +23,7 @@ function App() {
             <Preview markdown={text} />
         </div>
     );
+
 }
 
 function Preview({ markdown }) {
@@ -38,4 +39,39 @@ function Preview({ markdown }) {
         
 }
 
+const qwerty = `# freecodecamp Solutions:
+## Build a markdown previewer
+[Learn more about the freecodecamp.org project](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine).
+
+<dl>
+  <dt>Definition list</dt>
+  <dd>Is something people use sometimes.</dd>
+
+  <dt>Markdown in HTML</dt>
+  <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>
+</dl>
+
+\`This is an example of inline code\`
+
+\`\`\`\
+function exampleOf() {
+  return multiLineCodeBlock;
+}
+\`\`\`\
+
+- This
+- is
+- an
+- example
+- of
+- list
+- items
+
+> This is an example of block quote
+
+![freecodecamp](https://upload.wikimedia.org/wikipedia/commons/3/39/FreeCodeCamp_logo.png)
+
+**This is an example of Bolded Text**
+`;
 ReactDOM.render(<App />, document.getElementById("root"));
+
